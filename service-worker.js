659a6ee79d5dc12562d4f47eac9e1359e50fa0e1
34steve34 <stevesaad@gmail.com>
2026-02-16 22:15:52 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Star Stream PWA - VERSION 4.4.0 - 2026-02-16
-// gemini 4.4.0 added back random color explosion, added top single hit scores
+// Star Stream PWA - VERSION 4.5.0 - 2026-02-16
+// gemini 4.5.0 added back random color explosion, added top single hit scores
 // gemini fixing yaw and scope
 // 4.2.0 gemini moving scope; color coding target by distance; adding top scores
 // kiro gave up on gimbal lock 4.0.0 is the fix YAW in both playing positions
@@ -15,7 +15,7 @@
 // Bigger stars: THREE.PointsMaterial({ color: 0xffffff, size: 12, transparent: true })
 // FIXED: Removed all delta time, back to frame-locked physics
 
-const CACHE_NAME = 'star-stream-3D-v4.3.0';
+const CACHE_NAME = 'star-stream-3D-v4.5.0';
 const urlsToCache = [
   './',
   './index.html',
