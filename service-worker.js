712b5c81ed700c7fb6758b80657cfd4c64a5e66e
34steve34 <stevesaad@gmail.com>
2026-02-17 23:15:18 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 // Star Stream PWA - VERSION 4.6.2 - 2026-02-17
+// gemini 4.7.0 added explosion sound, added best single shot for current game
 // gemini 4.6.0 changed off screen target to include size/color
 // gemini 4.5.0 added back random color explosion, added top single hit scores
 // gemini fixing yaw and scope
@@ -16,7 +17,7 @@
 // Bigger stars: THREE.PointsMaterial({ color: 0xffffff, size: 12, transparent: true })
 // FIXED: Removed all delta time, back to frame-locked physics
 
-const CACHE_NAME = 'star-stream-3D-v4.6.2';
+const CACHE_NAME = 'star-stream-3D-v4.7.0';
 const urlsToCache = [
   './',
   './index.html',
